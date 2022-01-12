8f158d73b430d77f5868725ce1f41e062a9f810f
giselle-fs <giselle.fs@gmail.com>
2022-01-12 09:16:56 -0300

    Genero service and API endpoint added

--- src/app/cdd/classes/classes.component.ts
+++ src/app/cdd/classes/classes.component.ts
@@ -8,18 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClassesComponent implements OnInit {
 
-  livrosGeneros: Generos[] = [
-    { _idGenero: '0', nomeGenero: 'Generalidades', decimalGenero: '00', livrosGenero: 420 },
-    { _idGenero: '1', nomeGenero: 'Filosofia', decimalGenero: '00', livrosGenero: 105 },
-    { _idGenero: '2', nomeGenero: 'Religião', decimalGenero: '00', livrosGenero: 165 },
-    { _idGenero: '3', nomeGenero: 'Ciências Sociais', decimalGenero: '00', livrosGenero: 203 },
-    { _idGenero: '4', nomeGenero: 'Línguas', decimalGenero: '00', livrosGenero: 123 },
-    { _idGenero: '5', nomeGenero: 'Ciências Naturais e Matemática', decimalGenero: '00', livrosGenero: 523 },
-    { _idGenero: '6', nomeGenero: 'Tecnologia e Ciências Aplicadas', decimalGenero: '00', livrosGenero: 325 },
-    { _idGenero: '7', nomeGenero: 'Artes', decimalGenero: '00', livrosGenero: 523 },
-    { _idGenero: '8', nomeGenero: 'Literatura e Retórica', decimalGenero: '00', livrosGenero: 655 },
-    { _idGenero: '9', nomeGenero: 'Geografia História e Biografia', decimalGenero: '00', livrosGenero: 788 }
-  ]
+  livrosGeneros: Generos[] = []
 
   visaoColunas = ['_idGenero', 'nomeGenero', 'decimalGenero', ]
 
